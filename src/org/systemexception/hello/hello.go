dea0e1a8491b5c18dc5ad99524e7ca35112eb87d
Leonardo Cappuccio <leo@systemexception.org>
2016-12-02 21:42:31 +0100

    feed seed

--- src/org/systemexception/hello/hello.go
+++ src/org/systemexception/hello/hello.go
@@ -9,5 +9,6 @@ import (
 func main() {
 	fmt.Println("Hello Go!")
 	fmt.Println("The time is", time.Now())
+	rand.Seed(time.Now().UTC().UnixNano())
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
