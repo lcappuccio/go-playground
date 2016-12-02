b1fbd1c9b66d83835ae428df8d0d8d30f7386ae4
Leonardo Cappuccio <leo@systemexception.org>
2016-12-02 21:37:04 +0100

    add random

--- src/org/systemexception/hello/hello.go
+++ src/org/systemexception/hello/hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func main() {
-	fmt.Printf("Hello Go!\n")
+	fmt.Println("Hello Go!")
 	fmt.Println("The time is", time.Now())
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
